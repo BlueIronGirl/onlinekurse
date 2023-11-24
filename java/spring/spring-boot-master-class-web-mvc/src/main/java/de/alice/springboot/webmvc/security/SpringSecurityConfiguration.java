ecb18b3c795f22826ac10b994c33a681a070c854
Alice Bedow <alice.bedow@gmail.com>
2023-11-24 14:25:17 +0100

    Learn Spring Boot 3 in 100 Steps - No 1 Java Framework - Spring Security Hardcoded

--- java/spring/spring-boot-master-class-web-mvc/src/main/java/de/alice/springboot/webmvc/security/SpringSecurityConfiguration.java
+++ java/spring/spring-boot-master-class-web-mvc/src/main/java/de/alice/springboot/webmvc/security/SpringSecurityConfiguration.java
@@ -2,24 +2,43 @@ package de.alice.springboot.webmvc.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SpringSecurityConfiguration {
     @Bean
     public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
-        UserDetails userDetails = User.builder().passwordEncoder(passwordEncoder()::encode)
-                .username("alice").password("alice").roles("USER", "ADMIN").build();
+        UserDetails userDetails1 = createNewUser("alice", "alice");
+        UserDetails userDetails2 = createNewUser("test", "test");
 
-        return new InMemoryUserDetailsManager(userDetails);
+        return new InMemoryUserDetailsManager(userDetails1, userDetails2);
+    }
+
+    private UserDetails createNewUser(String username, String password) {
+        return User.builder().passwordEncoder(passwordEncoder()::encode)
+                .username(username).password(password).roles("USER", "ADMIN").build();
     }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity security) throws Exception {
+        security.authorizeHttpRequests(auth -> auth.anyRequest().authenticated()); // all requests must be authenticated
+        security.formLogin(Customizer.withDefaults()); // default login form
+        security.csrf(AbstractHttpConfigurer::disable); // disable CSRF (Cross-Site Request Forgery)
+        security.headers(customize -> customize.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable)); // disable X-Frame-Options (for H2-Console
+        return security.build();
     }
 }
