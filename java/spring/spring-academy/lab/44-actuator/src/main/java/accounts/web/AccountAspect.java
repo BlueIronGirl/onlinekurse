7cdf881e545948369314af13497f12a0476b1420
Alice Bedow <alice.bedow@gmail.com>
2024-03-01 15:04:49 +0100

    Spring Academy - Actuators, Metrics and Health Indicators Lab

--- java/spring/spring-academy/lab/44-actuator/src/main/java/accounts/web/AccountAspect.java
+++ java/spring/spring-academy/lab/44-actuator/src/main/java/accounts/web/AccountAspect.java
@@ -1,5 +1,13 @@
 package accounts.web;
 
+import accounts.AccountManager;
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
 /*
  * TODO-26 (Optional): Use AOP for counting logic
  * - Add `spring-boot-starter-aop` starter to the `pom.xml` or the
@@ -13,5 +21,23 @@ package accounts.web;
  * - Access `/accounts` several times and verify the metrics of
  *   `/actuator/metrics/account.fetch?tag=type:fromAspect
  */
+@Aspect
+@Component
 public class AccountAspect {
+    private Counter counter;
+
+    // TODO-08: Add a Micrometer Counter
+    // - Inject a MeterRegistry through constructor injection
+    //   (Modify the existing constructor below)
+    // - Create a Counter from the MeterRegistry: name the counter "account.fetch"
+    //   with a tag of "type"/"fromCode" key/value pair
+    @Autowired
+    public AccountAspect(MeterRegistry meterRegistry) {
+        this.counter = meterRegistry.counter("account.fetch", "type", "fromAspect");
+    }
+
+    @Before("execution(* accounts.web.AccountController.accountSummary(..))")
+    public void increment() {
+        counter.increment();
+    }
 }
