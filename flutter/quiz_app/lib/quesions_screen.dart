e3f0c012b33e619af023f26a26dacc346fd7d183
Alice Bedow <alice.bedow@gmail.com>
2024-07-04 11:22:45 +0200

    Quiz App - Questions

--- flutter/quiz_app/lib/quesions_screen.dart
+++ flutter/quiz_app/lib/quesions_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/answer_button.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -10,6 +11,18 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Text('QuestionsScreen');
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text('The question...'),
+          const SizedBox(height: 30),
+          AnswerButton(answerText: 'Answer1', onTap: () {}),
+          AnswerButton(answerText: 'Answer2', onTap: () {}),
+          AnswerButton(answerText: 'Answer3', onTap: () {}),
+        ],
+      ),
+    );
   }
 }
